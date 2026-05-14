3766cd6dda0d5cd3c9759ec273551c52420cc9be
Vivik1812 <an.verde@duocuc.cl>
2026-05-14 09:45:35 -0400

    fix: LoginView

--- src/modules/SYS/view/LoginView.jsx
+++ src/modules/SYS/view/LoginView.jsx
@@ -1,4 +1,4 @@
-import useLoginViewModel from '../viewModel/loginViewModel';
+import useLoginViewModel from '../viewModel/LoginViewModel';
 
 const LoginView = () => {
     const {email, setEmail, password, setPassword, error, cargando, iniciarSesion } = useLoginViewModel();
