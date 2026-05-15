90ce522f85356da3779b60717669ee0be6d7cb63
Vivik1812 <an.verde@duocuc.cl>
2026-05-15 09:44:24 -0400

    Feat: Nuevo footer

--- src/modules/SYS/view/components/Footer.jsx
+++ src/modules/SYS/view/components/Footer.jsx
@@ -1,45 +1,111 @@
+import { FaFacebookF, FaInstagram, FaTwitter, FaWhatsapp } from "react/fa";
+import { FaPaw } from "react-icons/fa6";
+
 export default function Footer() {
-    return (
-        <footer className="footer">
-            <div className= "footer-inner">
-                <div className= "footer-brand">
-                    <div className= "footer-logo">
-                        <svg>Aqui falta icono</svg>                        <div>
-                            <div className="footer-brand-name">Sanos y Salvos</div>
-                            <div className="footer-brand-tagline">NoseQuePonerAqui</div>
-                        </div>
-                    </div>
-                </div>
-                <p className= "footer-description">Plataforma colaborativa para ayudar a mascotas perdidas</p>
-                <div className= "footer-links">
-                    <a href="#" aria-label= "Facebook">
-                        <svg>Aqui falta icono</svg>
+  return (
+    <footer className="bg-light border-top mt-5 py-5">
+      <div className="container">
+        <div className="row gy-4">
+          <div className="col-lg-4">
+            <div className="d-flex aling-items-center mb-3">
+              <FaPaw size={35} className="text-success me-2" />
+              <div>
+                <h5 className="fw-bold text-success mb-0">Sanos & Salvos</h5>
+                <small className="fw-bold text-success mb-0">
+                  Unidos por ellos
+                </small>
+              </div>
+            </div>
+            <p className="text-muted mb-4">
+              Plataforma colaborativa para ayudar a encontrar y reunir mascotas
+              perdidas.
+            </p>
+            <div className="d-flex gap-3">
+                <a href="#" className="text-succes"><FaFacebookF size={18}/></a>
+                <a href="#" className="text-succes"><FaInstagram size={18}/></a>
+                <a href="#" className="text-succes"><FaTwitter size={18}/></a>
+                <a href="#" className="text-succes"><FaWhatsapp size={18}/></a>
+            </div>
+          </div>
+          <div className="col-6 col-md-4 col-lg-2">
+            <h6 className="fw-bold mb-3">
+                Sobre el proyecto
+            </h6>
+
+            <ul className="list-unstyled">
+                <li className="mb-3">
+                    <a href="#" className="text-decoration-none text-dark">
+                        ¿Quienes somos?
                     </a>
-                    <a href="#" aria-label="instagram">
-                        <svg >Aqui falta icono</svg>
+                </li>
+                <li className="mb-3">
+                    <a href="#" className="text-decoration-none text-dark">
+                        ¿Como funciona?
                     </a>
-                </div>
-            </div>
-            <div className= "footer-col">
-                <h4>Sobre Sanos y Salvos</h4>
-                <a href="#"> ¿Quienes somos?</a>
-                <a href="#"> Como funciona</a>
-                <a href="#"> Preguntas frecuentes</a>    
-            </div>
-            <div className= "footer-col">
-                <h4>Contacto</h4>
-                <a href="#"> Cantáctanos</a>
-                <a href="#"> SanosySalvos@gmail.com</a>
-                <a href="#"> p+56 9 1234 5678</a>    
-            </div>
-            <div className= "footer-col">
-                <h4>Términos</h4>
-                <a href="#"> Terminos y condiciones</a>
-                <a href="#"> política de privacidad</a>    
-            </div>
-        <div>
+                </li>
+                <li className="mb-3">
+                    <a href="#" className="text-decoration-none text-dark">
+                        ¿Preguntas frecuentes?
+                    </a>
+                </li>
+            </ul>
+          </div>
+          <div className="col-6 col-mb-4 col-lg-2">
+            <h6 className="fw-bold mb-3">
+                Ayuda
+            </h6>
+
+            <ul className="list-unstyled">
+                <li className="mb-2">
+                    <a href="#" className="text-decoration-none text-dark">Consejos</a>
+                </li>
+                <li className="mb-2">
+                    <a href="#" className="text-decoration-none text-dark">Que hacer</a>
+                </li>
+                <li className="mb-2">
+                    <a href="#" className="text-decoration-none text-dark">Como reportar</a>
+                </li>
+            </ul>
+          </div>
+          <div className="col-6 col-md-4 col-lg-2">
+            <h6 className="fw-bold mb-3">
+                Contacto
+            </h6>
+            <ul className="list-unstyled">
+                <li className="mb-2 text-muted">
+                    Contacnos
+                </li>
+                <li className="mb-2 text-muted">
+                    sanosysalvos@ayuda.cl
+                </li>
+                <li className="mb-2 text-muted">
+                    +56 9 1234 5678
+                </li>
+            </ul>
+          </div>
+          <div className="col-6 col-md-4 col-lg-2">
+            <h6 className="fw-bold mb-3">
+                Terminos
+            </h6>
+            <ul className="list-unstyled">
+                <li className="mb-2">
+                    <a href="#" className="text-decoration-none text-dark">
+                        Terminos y condiciones
+                    </a>
+                </li>
+                <li className="mb-2">
+                    <a href="#" className="text-decoration-none text-dark">
+                        Politica de privacidad
+                    </a>
+                </li>
+            </ul>
+          </div>
         </div>
-        <div className= "footer-bottom"> 2026 Sanos y Salvos. Todos los derechos reservados.</div>
+        <hr className="my-4"/>
+        <p className="text-center text-muted mb-0">
+            &copy; 2026 Sanos y Salvos. Todos los derechos revervador.
+        </p>
+      </div>
     </footer>
-    );
+  );
 }
